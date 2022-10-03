6603daba47cc31244de40b36ed71ebca9640132e
Terminal Tyrant <kubaska05@gmail.com>
2022-10-03 14:11:26 -0600

    Pog

--- install.bat
+++ install.bat
@@ -1,9 +1,10 @@
 @echo off
-if not exist C:%homepath%\AppData\Local\Programs\Git\git-cmd.exe echo Downloading Git-CMD... && powershell -Command "Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.37.3.windows.1/Git-2.37.3-64-bit.exe -OutFile C:%homepath%\Downloads\gitinstall.exe"
-if not exist C:%homepath%\AppData\Local\Programs\Git\git-cmd.exe echo Pick all defaults for the git installation: && C:%homepath%\Downloads\gitinstall.exe && pause
-if not exist C:%homepath%\vcpkg\vcpkg.exe echo Cloning vcpkg... && cd C:%homepath% && C:%homepath%\AppData\Local\Programs\Git\cmd\git clone https://github.com/microsoft/vcpkg && pause
-if not exist C:%homepath%\vcpkg\vcpkg.exe cd C:%homepath%\vcpkg && bootstrap-vcpkg.bat
-cd C:%homepath%\vcpkg
+if not exist %LOCALAPPDATA%\Programs\Git\cmd\git.exe echo Downloading Git-CMD... && powershell -Command "Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.37.3.windows.1/Git-2.37.3-64-bit.exe -OutFile C:%HOMEPATH%\Downloads\gitinstall.exe"
+if not exist %LOCALAPPDATA%\Programs\Git\cmd\git.exe echo Pick all defaults for the git installation: && C:%HOMEPATH%\Downloads\gitinstall.exe
+cd C:%HOMEPATH%
+if not exist C:%HOMEPATH%\vcpkg\vcpkg.exe echo Cloning vcpkg... && %LOCALAPPDATA%\Programs\Git\cmd\git.exe clone https://github.com/microsoft/vcpkg
+cd vcpkg
+if not exist C:%HOMEPATH%\vcpkg\vcpkg.exe echo Wait for bootstrap... && call bootstrap-vcpkg.bat
 vcpkg install sfml:x64-windows
 vcpkg integrate install
-pause
+PAUSE
