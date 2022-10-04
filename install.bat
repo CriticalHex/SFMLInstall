778750f57e9b238ded3cf2e25a02808a0fd3f037
Terminal Tyrant <kubaska05@gmail.com>
2022-10-04 12:22:44 -0600

    Avoid downloading git when unnecessary

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
-if not exist %LOCALAPPDATA%\Programs\Git\cmd\git.exe echo Downloading Git-CMD... && powershell -Command "Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.37.3.windows.1/Git-2.37.3-64-bit.exe -OutFile C:%HOMEPATH%\Downloads\gitinstall.exe"
-if not exist %LOCALAPPDATA%\Programs\Git\cmd\git.exe echo Pick all defaults for the git installation: && C:%HOMEPATH%\Downloads\gitinstall.exe
+if not exist %LOCALAPPDATA%\Programs\Git\cmd\git.exe if not exist C:%HOMEPATH%\Downloads\gitinstall.exe echo Downloading Git-CMD... && powershell -Command "Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.37.3.windows.1/Git-2.37.3-64-bit.exe -OutFile C:%HOMEPATH%\Downloads\gitinstall.exe"
+if not exist %LOCALAPPDATA%\Programs\Git\cmd\git.exe echo Pick all defaults for the git installation. && C:%HOMEPATH%\Downloads\gitinstall.exe
 cd C:%HOMEPATH%
 if not exist C:%HOMEPATH%\vcpkg\vcpkg.exe echo Cloning vcpkg... && %LOCALAPPDATA%\Programs\Git\cmd\git.exe clone https://github.com/microsoft/vcpkg
 cd vcpkg
