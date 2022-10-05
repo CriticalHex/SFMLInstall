561c2a1c70b10c48db5e116b366dceea11479096
Terminal Tyrant <kubaska05@gmail.com>
2022-10-05 12:51:31 -0600

    Completely untested, using goto.

--- uninstall.bat
+++ uninstall.bat
@@ -1,7 +1,29 @@
 @echo off
+goto check_Permissions
+
+:check_Permissions
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        goto admin
+    ) else (
+        goto user
+    )
+
+:admin
 C:%homepath%\vcpkg\vcpkg remove sfml:x64-windows
 rmdir C:%homepath%\vcpkg /S /Q
-rmdir C:%homepath%\AppData\Local\vcpkg /S /Q
-C:%homepath%\AppData\Local\Programs\Git\unins000.exe
-rmdir C:%homepath%\AppData\Local\Programs\Git /S /Q
-pause
+rmdir %LOCALAPPDATA%\vcpkg /S /Q
+"%ProgramFiles%\Git\unins000.exe"
+rmdir "%ProgramFiles%\Git" /S /Q
+goto end
+
+:user
+C:%homepath%\vcpkg\vcpkg remove sfml:x64-windows
+rmdir C:%homepath%\vcpkg /S /Q
+rmdir %LOCALAPPDATA%\vcpkg /S /Q
+%LOCALAPPDATA%\Programs\Git\unins000.exe
+rmdir %LOCALAPPDATA%\Programs\Git /S /Q
+goto end
+
+:end
+PAUSE
